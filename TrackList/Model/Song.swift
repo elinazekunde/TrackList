c9e6741c9ae557bbcc31279a1e9ae9baa229166d
Elina <elina.zekunde@gmail.com>
2021-02-03 16:17:24 +0200

    track list table view project

---
+++ TrackList/Model/Song.swift
@@ -0,0 +1,28 @@
+//
+//  Song.swift
+//  TrackList
+//
+//  Created by Elīna Zekunde on 03/02/2021.
+//
+
+import Foundation
+
+struct Song {
+    
+    let track: String
+    let album: String
+    let cover: String
+    
+    static func createSong() -> [Song] {
+        var songs: [Song] = []
+        let tracks = DataManager.shared.track
+        let albums = DataManager.shared.album
+        let covers = DataManager.shared.cover
+        
+        for i in 0..<tracks.count {
+            let song = Song(track: tracks[i], album: albums[i], cover: covers[i])
+            songs.append(song)
+        }
+        return songs
+    }
+}
